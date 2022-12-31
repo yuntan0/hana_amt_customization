ca9c3fe0bc0198b29339193803025d0fd9d3e7c4
John Park <yuntan0@gmail.com>
2022-12-31 09:46:01 +0000

    경영관리 부서 게시판 추가

---
+++ hana_amt_customization/hana_amt_customization/doctype/admin_bulletin_board/admin_bulletin_board.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, John and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Admin Bulletin Board', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ hana_amt_customization/hana_amt_customization/doctype/admin_bulletin_board/test_admin_bulletin_board.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, John and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAdminBulletinBoard(FrappeTestCase):
+	pass
